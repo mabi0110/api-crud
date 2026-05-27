b6e559590fd266a9204c1b35a68c1584a95de544
mabi0110 <mariadb0110gmail.com>
2026-05-27 13:26:14 +0200

    initial commit

---
+++ src/test/java/pl/javastart/apicrud/ApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.apicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
