1b4db5eea030eb76798d420001b3d75b1f485415
mabi0110 <mariadb0110gmail.com>
2026-05-31 14:23:03 +0200

    add ResponseEntity to controller

--- src/main/java/pl/javastart/apicrud/JobOfferController.java
+++ src/main/java/pl/javastart/apicrud/JobOfferController.java
@@ -1,10 +1,10 @@
 package pl.javastart.apicrud;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 public class JobOfferController {
@@ -17,7 +17,9 @@ public class JobOfferController {
 
 
     @GetMapping("/offers/{id}")
-    Optional<JobOfferDto> getJobOfferById(@PathVariable Long id) {
-        return jobOfferService.getJobOfferById(id);
+    ResponseEntity<JobOfferDto> getJobOfferById(@PathVariable Long id) {
+        return jobOfferService.getJobOfferById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
